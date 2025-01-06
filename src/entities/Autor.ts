e5727809c76b12f6a06c990645f4619af3984722
eujoaodev@gmail.com <eujoaodev@gmail.com>
2025-01-06 18:43:55 -0300

    feat: criar entidade Autor para gerenciamento de autores

--- src/entities/Autor.ts
+++ src/entities/Autor.ts
@@ -1,5 +1,34 @@
-class Autor {
-    
-}
-
-export default Autor;
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    CreateDateColumn,
+    UpdateDateColumn,
+  } from "typeorm";
+  
+  @Entity("autores") // Nome da tabela no banco
+  export class Autor {
+    @PrimaryGeneratedColumn()
+    id: number;
+  
+    @Column({ type: "varchar", nullable: false })
+    name: string;
+  
+    @Column({ type: "date", nullable: false })
+    birthdate: Date;
+  
+    @Column({ type: "text", nullable: true })
+    biography: string;
+  
+    @Column({ type: "varchar", nullable: false })
+    nationality: string;
+  
+    @Column({ type: "boolean", default: true })
+    active: boolean;
+  
+    @CreateDateColumn()
+    created_at: Date;
+  
+    @UpdateDateColumn()
+    updated_at: Date;
+  }
