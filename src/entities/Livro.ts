6c23bb1f70dd95c7e9eb7dae8b7be3d329c88d1c
tiago <tiago.dscouto@hotmail.com>
2025-01-13 17:01:30 -0300

    update entities books

--- src/entities/Livro.ts
+++ src/entities/Livro.ts
@@ -1,6 +1,38 @@
 
-class Livro {
- 
-}
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    CreateDateColumn,
+    UpdateDateColumn,
+} from "typeorm";
 
-export default Livro;
+@Entity("books") // Nome da tabela no banco
+export class Livro {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ type: "varchar", nullable: false })
+    title: string;
+
+    @Column({ type: "text", nullable: false })
+    description: string;
+
+    @Column({ type: "date", nullable: true })
+    publication_date: Date;
+
+    @Column({ type: "varchar", nullable: false })
+    isbn: string;
+
+    @Column({ type: "int", default: false })
+    page_count: number;
+
+    @Column({ type: "varchar", default: false })
+    language: string;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @UpdateDateColumn()
+    updated_at: Date;
+}
--- src/routes/livro.routes.ts
+++ src/routes/livro.routes.ts
@@ -1,7 +1,12 @@
 import { Request, Response, Router } from 'express';
+import { getRepository } from "typeorm";
+import { Livro } from "../entities/Livro";
 
 const livroRoutes = Router();
 
 
 
 export default livroRoutes;
+
+
+
