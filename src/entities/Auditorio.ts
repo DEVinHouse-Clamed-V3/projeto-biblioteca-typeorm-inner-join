a20ddf39fe9e477b4cff0ad1e78c0d3ca8787d3c
Gabriel <rev3npvp@gmail.com>
2025-01-17 14:00:12 -0300

    criacao de entidades

--- src/entities/Auditorio.ts
+++ src/entities/Auditorio.ts
@@ -1,5 +1,36 @@
-class Auditorio {
-    
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@Entity("auditoriums")
+class Auditorium {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  capacity: number;
+
+  @Column()
+  location: string;
+
+  @Column({ default: false })
+  has_projector: boolean;
+
+  @Column({ default: false })
+  has_sound_system: boolean;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
 }
 
-export default Auditorio;
+export default Auditorium;
