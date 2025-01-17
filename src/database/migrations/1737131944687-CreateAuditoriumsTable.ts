c419a73c00eca1d0f2134aed5bddd628e3f4218e
Gabriel <rev3npvp@gmail.com>
2025-01-17 13:42:31 -0300

    criação de migrations auditorios

---
+++ src/database/migrations/1737131944687-CreateAuditoriumsTable.ts
@@ -0,0 +1,31 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateAuditoriumsTable implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "auditoriums",
+        columns: [
+          {
+            name: "id",
+            type: "int",
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: "increment",
+          },
+          { name: "name", type: "varchar", isNullable: false },
+          { name: "capacity", type: "int", isNullable: false },
+          { name: "location", type: "varchar", isNullable: false },
+          { name: "has_projector", type: "boolean", default: false },
+          { name: "has_sound_system", type: "boolean", default: false },
+          { name: "created_at", type: "timestamp", default: "now()" },
+          { name: "updated_at", type: "timestamp", default: "now()" },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("auditoriums");
+  }
+}
