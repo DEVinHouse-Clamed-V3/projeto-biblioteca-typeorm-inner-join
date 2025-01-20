f19f7c17f9d582284382b2c288aadb2aebe872ee
nataliaclem <nataliaclementinosilva@gamil.com>
2025-01-20 08:27:33 -0300

    model entidade

--- src/entities/Leitor.ts
+++ src/entities/Leitor.ts
@@ -1,5 +1,39 @@
-class Leitor {
-    
-}
-
-export default Leitor;
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    CreateDateColumn,
+    UpdateDateColumn,
+  } from "typeorm";
+  
+  @Entity("leitores")
+  class Leitores {
+    @PrimaryGeneratedColumn()
+    id: number;
+  
+    @Column()
+    name: string;
+  
+    @Column()
+    email: string;
+  
+    @Column()
+    phone_number: string;
+  
+    @Column()
+    birthdate: Date;
+  
+    @Column()
+    address: string;
+  
+    @Column({ default: true })
+    active: boolean;
+  
+    @CreateDateColumn()
+    created_at: Date;
+  
+    @UpdateDateColumn()
+    updated_at: Date;
+  }
+  
+  export default Leitores;
